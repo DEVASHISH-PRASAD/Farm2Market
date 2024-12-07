a942bc28fac07d67011dda9d79e0339a2b6fcab3
Dev <128153292+DEVASHISH-PRASAD@users.noreply.github.com>
2024-12-07 10:56:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.jsx
 import React, { useEffect } from "react";
 import HomePage from "./Pages/HomePage";
 import "aos/dist/aos.css";
 import Signup from "./Pages/Signup";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"  // Changed BrowserRouter to HashRouter
 import VegetablesPage from "./Pages/Products/VegetablePage";
 import CartPage from "./Pages/CartPage";
 import FruitsPage from "./Pages/Products/FruitsPage";
@@ -19,7 +18,7 @@ import RequireAuth from "./components/RequireAuth";
 
 function App() {
   return (
-    <>
+    <Router> {/* Wrap the Routes in Router (HashRouter now instead of BrowserRouter) */}
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
 
@@ -28,28 +27,25 @@ function App() {
             <RequireAuth allowedRoles={["ADMIN", "CUSTOMER", "FARMER"]} />
           }
         >
-          <Route path="/dashboard" element={<Dashboard/>}></Route>
-          <Route path="/cart" element={<CartPage />}></Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cart" element={<CartPage />} />
         </Route>
 
-        <Route path="/aboutUs" element={<AboutUs />}></Route>
-        <Route path="/products" element={<ProductPage />}></Route>
-        <Route path="/contactUs" element={<ContactUs />}></Route>
-
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-
-        <Route path="/vegetable" element={<VegetablesPage />}></Route>
-        <Route path="/fruits" element={<FruitsPage />}></Route>
-        <Route path="/grains" element={<GrainsPage />}></Route>
-
-        <Route path="/denied" element={<AccessDenied />}></Route>
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/vegetable" element={<VegetablesPage />} />
+        <Route path="/fruits" element={<FruitsPage />} />
+        <Route path="/grains" element={<GrainsPage />} />
+        <Route path="/denied" element={<AccessDenied />} />
 
         <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
-          <Route path="/createItem" element={<CreateItem />}></Route>
+          <Route path="/createItem" element={<CreateItem />} />
         </Route>
       </Routes>
-    </>
+    </Router>  {/* Ensure Routes are wrapped with HashRouter (Router now wraps the Routes) */}
   );
 }
 
