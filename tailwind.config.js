3ab26c8a2a46f028e69fc8920aab8bd3ede1e972
Dev <128153292+DEVASHISH-PRASAD@users.noreply.github.com>
2024-12-07 11:56:35 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-
 // tailwind.config.js
+import daisyui from 'daisyui';
+
 export const content = [
   "./src/**/*.{js,jsx,ts,tsx}",
   "./node_modules/daisyui/dist/**/*.js",
 ];
+
 export const theme = {
   extend: {},
 };
-export const plugins = [require("daisyui")];
+
+export const plugins = [daisyui];
+
 export const daisyui = {
   themes: ["light", "dark"], // you can add more themes here
 };
